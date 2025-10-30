7c4a2337574e38a44012922a40ab66cc9ec2ddfa
unknown <ayoub1benmansour@gmail.com>
2025-10-30 10:59:30 +0100

    Added util classes to manage the controller response and exception

---
+++ src/main/java/ma/youcode/shared/util/GlobalResponseHandler.java
@@ -0,0 +1,36 @@
+package ma.youcode.shared.util;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType,
+                            Class<? extends HttpMessageConverter<?>> converterType) {
+        // Apply this to all controllers except when ResponseEntity is already returned
+        return !returnType.getParameterType().equals(org.springframework.http.ResponseEntity.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        // If the response is already an ApiResponse, don't wrap it again
+        if (body instanceof ApiResponse)
+            return body;
+
+        // Otherwise, wrap it
+        return ApiResponse.success(body);
+    }
+}
+
